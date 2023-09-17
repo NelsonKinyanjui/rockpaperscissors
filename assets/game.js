cd8c555758505daedea23dea3d65f5265f3046b6
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-17 22:11:00 +0300

    add a switch statement

--- assets/game.js
+++ assets/game.js
@@ -1,5 +1,12 @@
 function getComputerChoice(){
-  result = 'rock' ,'paper' ,'scissors';
-  return result;
+    let result = math.floor(math.random() * 3);
+   switch (result){
+    case 0:
+    return 'rock';
+    case 1:
+        return 'paper';
+        case 2:
+        return 'scissors';
+   }
 }
 console.log(getComputerChoice());
