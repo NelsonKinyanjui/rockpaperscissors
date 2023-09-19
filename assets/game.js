e6dcd17b068061cd2f3ce7585ac7fcbb3244e363
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-19 07:04:53 +0300

    add a loop

--- assets/game.js
+++ assets/game.js
@@ -1,20 +1,27 @@
- function getComputerChoice(){
 
-    let result = Math.floor(Math.random() * 3);
-   switch (result){
-    case 0:
-    return 'rock';
-    case 1:
-        return 'paper';
-        case 2:
-        return 'scissors';
-   }
-}
-const computerSelection = getComputerChoice();
-console.log(getComputerChoice());
- const playerSelection = 'rock';
+function game(){
+    for(i = 0 ; i <= 5; i++){
+        function getComputerChoice(computerSelection){
 
-function playRound(payerSelection,computerSelection){
-return `you loose! ${computerSelection} beats ${playerSelection} `;
+            let result = Math.floor(Math.random() * 3);
+           switch (result){
+            case 0:
+            return 'rock';
+            case 1:
+                return 'paper';
+                case 2:
+                return 'scissors';
+           }
+        }
+        
+        console.log(getComputerChoice());
+         const playerSelection = prompt('');
+        let computerSelection = getComputerChoice();
+        function playRound(payerSelection,computerSelection){
+        return `you loose! ${computerSelection} beats ${playerSelection} `;
+        }
+        //console.log(playRound(playerSelection,computerSelection));
+
+    }
 }
-console.log(playRound());
+console.log(game());
