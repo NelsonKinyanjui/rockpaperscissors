04cab7bbfd4728c52523198b308dfe03f775b473
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-21 21:57:02 +0300

    add computer selection so as the next task will be player selectiona nd the game will be complete

--- assets/game.js
+++ assets/game.js
@@ -1,4 +1,17 @@
 
 function getComputerChoice(){
+    const randomNumber= Math.floor(Math.random() * 2);
+    switch(randomNumber){
+        case 0:
+        return 'rock';
+        
+        case 1:
+        return 'paper';
+        
+        case 2:
+        return 'scissors';
     
+    }
 }
+const computerSelection = getComputerChoice();
+console.log(`computer choice is ${computerSelection}`);
